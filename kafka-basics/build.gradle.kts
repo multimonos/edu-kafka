a1bad7cbe83b81576550f5aaf84484b0937ec50b
Craig Hopgood <searaig@gmail.com>
2025-02-10 18:11:34 -0500

    add deps kafka clients slf4j and slf4j-simple

--- kafka-basics/build.gradle.kts
+++ kafka-basics/build.gradle.kts
@@ -10,8 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.9.0")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.16")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
